621425475107746230888cb5b4ba8627eba82215
znamenskiialeksei <153446608+znamenskiialeksei@users.noreply.github.com>
2025-09-06 01:28:29 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -100,9 +100,14 @@ function createElement(elementData) {
                 element.appendChild(dropdownMenu);
                 button.addEventListener('click', () => {
                     dropdownMenu.classList.toggle('show');
+                    // Задача 2: "Умное" позиционирование
                     const rect = dropdownMenu.getBoundingClientRect();
-                    if (rect.right > window.innerWidth) dropdownMenu.classList.add('dropdown-menu-right');
-                    if (rect.left < 0) dropdownMenu.classList.remove('dropdown-menu-right');
+                    if (rect.right > window.innerWidth) {
+                        dropdownMenu.classList.add('dropdown-menu-right');
+                    }
+                    if (rect.left < 0) {
+                        dropdownMenu.classList.remove('dropdown-menu-right');
+                    }
                 });
                 window.addEventListener('click', (e) => { if (!element.contains(e.target)) dropdownMenu.classList.remove('show'); });
             } else {
