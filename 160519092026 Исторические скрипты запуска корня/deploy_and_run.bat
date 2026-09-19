2a69bbce1efe4c2df031fe29e409a8871f79c33a
Your Name <you@example.com>
2026-09-20 00:27:22 +0300

    spark: clean production tree and content sync - 2026-09-20 00:27:21

--- 160519092026 Исторические скрипты запуска корня/deploy_and_run.bat
+++
@@ -1,1 +0,0 @@
-:: deploy_and_run.bat ликвидирован по прямому указанию пользователя. Используйте нативный deploy_and_run.ps1 в PowerShell VS Code или запуск через задачи VS Code (.vscode/tasks.json).
